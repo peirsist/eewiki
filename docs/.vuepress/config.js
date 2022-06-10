3833ebeb198f97f3b5df154aeb9d3f9aae4e3a45
peirsist <1210339631@qq.com>
2022-06-10 11:48:11 +0800

    first commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,7 +13,7 @@ module.exports = {
   // 如果GitHub Pages绑定域名，那么配置文件中的base参数则需要删除，否则域名访问会失败。
   // base : '/cs-wiki/',
   head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
-    ['link', { rel: 'icon', href: '/img/111.ico' }], //favicons，资源放在public文件夹
+    ['link', { rel: 'icon', href: '/img/222.ico' }], //favicons，资源放在public文件夹
     ['meta', { name: 'keywords', content: 'C++,algorithm,interview' }],
     ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
     // ['meta', { name: 'wwads-cn-verify', content: '6c4b761a28b734fe93831e3fb400ce87' }], // 广告相关，你可以去掉
